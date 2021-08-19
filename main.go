01b1e800b4cd7a0dd5bb26e8335b3eec789cbb25
daijinlong <daijinlong@84852>
2021-08-19 10:14:12 +0800

    工具，入口

--- main.go
+++ main.go
@@ -3,23 +3,46 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 	"go-gin-admin/common"
+	"go-gin-admin/middleware"
+	"os"
 )
 
 func main() {
-
+	InitConfig()
 	common.InitDB()
 
 	// 1.创建路由
 	router := gin.Default()
 
+	router.Use(middleware.CommonMiddleware())
+
 	// 2.绑定路由规则，执行的函数
 	// gin.Context，封装了request和response
 	router = CollectRoute(router)
 
+	prot := viper.GetString("server.port")
+	if prot != "" {
+		// Run("里面不指定端口号默认为8080")
+		fmt.Printf("start server (port:%s)", prot)
+		panic(router.Run(":" + prot))
+	}
 	// 3.监听端口，默认在8080
-	// Run("里面不指定端口号默认为8080")
-	fmt.Printf("start server (port:%s)", "8000")
-	router.Run(":8000")
+	panic(router.Run())
+}
 
+func InitConfig() {
+	workDir, _ := os.Getwd()
+	//设置要读取的文件名
+	viper.SetConfigName("application")
+	//设置文件读取类型
+	viper.SetConfigType("yml")
+	//设置文件的路径
+	viper.AddConfigPath(workDir + "/config")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 }
--- util/util.go
+++ util/util.go
@@ -1,7 +1,12 @@
 package util
 
 import (
+	"crypto/md5"
+	"fmt"
+	"io"
+	"log"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -13,4 +18,43 @@ func RandGetName(n int) string {
 		result[i] = letters[rand.Intn(len(letters))]
 	}
 	return string(result)
+}
+
+func IsContainStr(items []string, item string) bool {
+	for _, eachItem := range items {
+		if eachItem == item {
+			return true
+		}
+	}
+	return false
+}
+
+/**
+获取MD5
+*/
+func GetMd5String(str string) string {
+	m := md5.New()
+	_, err := io.WriteString(m, str)
+	if err != nil {
+		log.Fatal(err)
+	}
+	arr := m.Sum(nil)
+	return fmt.Sprintf("%x", arr)
+}
+
+/**
+检查文件或文件夹是否存在
+1.如果返回的错误为nil,说明文件或文件夹存在
+2.如果返回的错误类型使用os.IsNotExist()判断为true,说明文件或文件夹不存在
+3.如果返回的错误为其它类型,则不确定是否在存在
+*/
+func IsFileExist(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
 }
